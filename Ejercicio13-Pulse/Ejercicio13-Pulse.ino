a230ae3dc109f09995f709d48e715ebff37ca758
Crufo <csrufo1@gmail.com>
2019-09-11 19:03:58 +0200

    Ejercicio 13

---
+++ Ejercicio13-Pulse/Ejercicio13-Pulse.ino
@@ -0,0 +1,66 @@
+/*
+   Ejercicio13 - Pulse
+   Web: https://www.aprendiendoarduino.com/cursos/programacion-arduino-2019/
+   Montaje: https://github.com/jecrespo/aprendiendoarduino-Curso_Programacion_Arduino_2019/blob/master/Esquemas%20Montajes%20Pr%C3%A1cticas%20Fritzing/Curso_Programacion-Montaje%20Arduino_bb.png
+*/
+
+//Definición pines
+#define PIN_BOTON_A 2
+#define PIN_LED_1 4
+#define PIN_LED_2 5 //PWM
+#define PIN_LED_3 6 //PWM
+#define PIN_LED_4 7
+
+//Variables globales
+unsigned long duracion = 0;
+boolean estado_luces = 0; //0 = apagado, 1 = encendido
+byte pin_encendido = PIN_LED_1;
+
+void setup() {
+  //Inicializo puerto serie
+  Serial.begin(9600);
+
+  //Inicializo Pines
+  Serial.println(F("Inicializando pines digitales..."));
+  pinMode(PIN_BOTON_A, INPUT_PULLUP);
+  pinMode(PIN_LED_1, OUTPUT);
+  pinMode(PIN_LED_2, OUTPUT);
+  pinMode(PIN_LED_3, OUTPUT);
+  pinMode(PIN_LED_4, OUTPUT);
+
+}
+
+void loop() {
+  duracion = pulseInLong(PIN_BOTON_A, LOW, 20000000); //el programa se bloquea en este punto
+  Serial.println(duracion / 1000); //en milisegundos
+  delay(50);
+  if (duracion == 0) {
+    Serial.println("pulsacion no detectada en 20 segundos");
+  }
+  else if (duracion > 2000000) {
+    Serial.println("pulsacion larga");
+    if (estado_luces == 0) {
+      estado_luces = 1;
+      digitalWrite(pin_encendido, HIGH);
+      Serial.println("Enciendo pin " + (String)pin_encendido);
+    }
+    else {
+      estado_luces = 0;
+      digitalWrite(pin_encendido, LOW);
+      Serial.println("Apago Leds");
+      pin_encendido = PIN_LED_1; //inicializo el led a iluminar
+    }
+  }
+  else {
+    Serial.println("pulsacion corta");
+    if (estado_luces == 1) {
+      digitalWrite(pin_encendido, LOW);
+      Serial.println("Apago pin " + (String)pin_encendido);
+      pin_encendido++;
+      if (pin_encendido > PIN_LED_4)
+        pin_encendido = PIN_LED_1;
+      digitalWrite(pin_encendido, HIGH);
+      Serial.println("Enciendo pin " + (String)pin_encendido);
+    }
+  }
+}
